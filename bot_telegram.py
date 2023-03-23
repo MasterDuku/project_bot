9d09f32374aefd1a3869417453a41d76c34dc929
Master Duku <galama99@gmail.com>
2023-03-23 19:17:20 +0300

    Add new methods in bot

--- bot_telegram.py
+++ bot_telegram.py
@@ -10,15 +10,40 @@ load_dotenv(find_dotenv())
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
+
+"""****************************Клиентская часть************************************"""
+@dp.message_handler(commands=['start','help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Привет! Я бот и мнен интересно с тобой общаться.')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/Test_my_project')
+
+@dp.message_handler(commands=['Расположение'])
+async def pizza_place_command(message: types.Message):
+    await bot.send_message(message.from_user.id, text='Иди туда, где вкусно пахнет,\nна улицу Космонавтов д.8')
+
+@dp.message_handler(commands=['working'])
+async def pizza_open_command(message: types.Message):
+    await bot.send_message(message.from_user.id, text='Вс-Чт с 9:00 до 20:00, Пт-Сб с 9:00 до 23:00')
+
+
+"""****************************Админская часть************************************"""
+
+
 async def on_startup(_):
     print('--'*4,'Bot started', '--'*4)
 
-
+"""****************************Общая часть************************************"""
 @dp.message_handler()
 async def echo_send(message: types.Message):
+
+
+    
     # await message.answer(message.text)
     # await message.reply(message.text)
-    await bot.send_message(message.from_user.id, message.text)
+    # await bot.send_message(message.from_user.id, message.text)
 
 
 
